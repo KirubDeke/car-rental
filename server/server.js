94c5f16c177f50cc86ff899da572fcf937646d01
KirubDeke <fistumkirubeldeke@gmail.com>
2025-08-20 22:29:20 +0300

    cors solved

--- server/server.js
+++ server/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 8000;
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://localhost:3001"
+  "http://localhost:3001",
+  "https://kirub-rental.vercel.app/"
   // "https://curious-life.vercel.app"
 ];
 
