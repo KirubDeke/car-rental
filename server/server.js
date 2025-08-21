a44d87474b2e13916962134307fcb5623235772e
KirubDeke <fistumkirubeldeke@gmail.com>
2025-08-21 12:46:15 +0300

    / removed

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://kirub-rental.vercel.app/"
+  "https://kirub-rental.vercel.app",
   // "https://curious-life.vercel.app"
 ];
 
